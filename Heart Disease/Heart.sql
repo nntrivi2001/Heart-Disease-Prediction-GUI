ad59c2e9985394401c7be8f05c1a17d68a61d1e1
Nguyễn Ngọc Trí Vĩ <nntrivi2001@gmail.com>
2023-06-07 17:06:50 +0700

    Public

---
+++ Heart Disease/Heart.sql
@@ -0,0 +1,44 @@
+Create Database Heart
+Go
+
+Use Heart 
+Go
+
+Create Table Patient (
+	id Char(13) Primary Key,
+	fullname Nvarchar(50),
+)
+
+CREATE TABLE MedicalRecords (
+	id char(13) Primary Key,
+    age INT,
+    sex BIT,
+    trestbps INT,
+    chol INT,
+    fbs INT,
+    restecg INT,
+    thalach INT,
+    exang INT,
+    oldpeak FLOAT,
+    ca INT,
+    typical_angina_pain BIT,
+    atypical_angina_pain BIT,
+    non_anginal_pain BIT,
+    asymptomatic_pain BIT,
+    thalassemia_normal BIT,
+    thalassemia_fixed BIT,
+    thalassemia_reversable BIT,
+    upsloping_st BIT,
+    flat_st BIT,
+    downsloping_st BIT,
+	heart_disease BIT
+);
+Go
+
+Select p.id, fullname, age, sex, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, ca, typical_angina_pain, atypical_angina_pain, non_anginal_pain, asymptomatic_pain, thalassemia_normal, thalassemia_fixed, thalassemia_reversable, upsloping_st, flat_st, downsloping_st, heart_disease
+From Patient p, MedicalRecords mr 
+Where mr.id = p.id
+
+select * from patient
+insert into Patient Values('123', 'Vu Khanh Nam')
+insert into MedicalRecords Values('123', 63, 1, 145, 233, 1, 0, 150, 0, 2.3, 2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1)
